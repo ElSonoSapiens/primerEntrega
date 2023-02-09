f5a837c5ada1d4b4351472c4faae03f0ff37c1a1
Diego Hernandez <dihf.47@gmail.com>
2023-02-08 22:05:34 -0300

    after Clase

---
+++ Clase 01/index.js
@@ -0,0 +1,26 @@
+// actividad en clase
+
+// let nombre = "Diego"
+// let edad = 33
+// let precio = 1000
+// let nombreSeries = ["breakingBad","TheOffice","Jackass"]
+// let nombrePeliculas = ["Gladiador","SpiderMan", "Matrix"]
+
+let usuarios =[
+    {
+    nombre:"Ramon",
+    edad : 33,
+    precio: 1000,
+    nombreSeries: ["breakingBad","TheOffice","Jackass"],
+    nombrePeliculas : ["Gladiador","SpiderMan", "Matrix"]
+    },
+    {
+    nombre:"Lis",
+    edad : 35,
+    precio: 1500,
+    nombreSeries: ["Emily","TheOffice","JarriPoter"],
+    nombrePeliculas : ["Sascuach","SpiderMan", "Everlong"]
+    }
+]
+
+console.log("hola");
