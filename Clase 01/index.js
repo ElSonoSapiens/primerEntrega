c31c40158e488f7fe9c6299f088e770603afc3bb
Diego Hernandez <dihf.47@gmail.com>
2023-02-14 15:17:22 -0300

    14-02

--- Clase 01/index.js
+++ Clase 01/index.js
@@ -25,3 +25,20 @@ let usuarios =[
 
 console.log("hola");
 console.log(usuarios.edad)
+
+//---------------- Repaso de info => .map
+
+let numeros = [1,2,3,4,5,6,7,8,9]
+let dobles = numeros.map(function(x){
+    return x*2;
+})
+
+console.log(numeros)
+console.log(dobles)
+
+let expoDobles = dobles.map((a,b)=>b**a)
+let expoDobles2 = dobles.map((a,b)=>a**b)
+
+console.log(expoDobles)
+
+console.log(expoDobles2)
--- Clase 2/index.js
+++ Clase 2/index.js
@@ -1,14 +1,14 @@
-class Persona{
-    constructor(nombre){
-        this.nombre = nombre
-    }
-static especie ="humano";
-saludar = () =>{
-console.log(`Hola, soy ${this.nombre}, un guston`)
-}
-getEspecie = () => {
-    console.log(`Aunque no parezca, soy un ${Persona.especie}`)
-}
+class Persona {
+	constructor(nombre) {
+		this.nombre = nombre;
+	}
+	static especie = "humano";
+	saludar = () => {
+		console.log(`Hola, soy ${this.nombre}, un guston`);
+	};
+	getEspecie = () => {
+		console.log(`Aunque no parezca, soy un ${Persona.especie}`);
+	};
 }
 
 let persona1 = new Persona("Diego");
@@ -22,46 +22,56 @@ persona2.getEspecie();
 
 //--------------------- ES7 - exponencial ** - array.includes
 
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let dobles = numeros.map(function (x) {
+	return x * 2;
+});
 
-let nombres = ["Diego","Lis","Juan","Flor","ELi"]
+console.log(numeros);
+console.log(dobles);
 
-if(nombres.includes("Dieg")){
-    console.log("Diego está en el array")
-}
-else{
-    console.log("No se encuentra")
+let expoDobles = dobles.map((a, b) => b ** a);
+let expoDobles2 = dobles.map((a, b) => a ** b);
+
+console.log(expoDobles);
+
+console.log(expoDobles2);
+
+//-- INCLUDES
+
+let nombres = ["Diego", "Lis", "Juan", "Flor", "ELi"];
+
+if (nombres.includes("Diego")) {
+	console.log("Diego está en el array");
+} else {
+	console.log("No se encuentra");
 }
 
 //-------------------- ES8 async await - Object.entries values y keys
 
-let numLet ={
-    Nombre:"Diego",
-    Edad:33,
-    Casado:false
-}
-let numLet2 ={Nombre:"Lissie",Edad:31,Casado:false} 
+let numLet = {
+	Nombre: "Diego",
+	Edad: 33,
+	Casado: false,
+};
+let numLet2 = {
+	Nombre: "Lissie",
+	Edad: 31,
+	Casado: false,
+};
 
 let parLlaveValor = Object.entries(numLet);
 console.log(parLlaveValor);
 
-let propsOnly = Object.keys(numLet)
+let parLlaveValor2 = Object.entries(numLet2);
+console.log(parLlaveValor2);
+
+let propsOnly = Object.keys(numLet);
 console.log(propsOnly);
 
-let valueOnly = Object.values(numLet)
+let valueOnly = Object.values(numLet);
 console.log(valueOnly);
 
-//------------------- ES 9 .finally() - rest y spread
+let reduceValues = valueOnly.reduce((a, b) => a + b);
+console.log(reduceValues);
 
-let {Nombre} = numLet;
-let numLet3 = {...numLet,...numLet2}
-
-console.log(numLet3)
-
-let numLet4 ={
-    a:1,
-    b:2,
-    c:3
-}
-
-let{a,...rest} = numLet4
-console.log(rest)
