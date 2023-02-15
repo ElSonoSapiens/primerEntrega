0063e0ad9505592c366c21559733f4ba30869d46
ElSonoSapiens <dihf.47@gmail.com>
2023-02-15 01:04:11 -0300

    Repasando clase 1

--- Clase 01/index.js
+++ Clase 01/index.js
@@ -1,44 +1,17 @@
-// actividad en clase
-
-// let nombre = "Diego"
-// let edad = 33
-// let precio = 1000
-// let nombreSeries = ["breakingBad","TheOffice","Jackass"]
-// let nombrePeliculas = ["Gladiador","SpiderMan", "Matrix"]
-
-let usuarios =[
-    {
-    nombre:"Ramon",
-    edad : 33,
-    precio: 1000,
-    nombreSeries: ["breakingBad","TheOffice","Jackass"],
-    nombrePeliculas : ["Gladiador","SpiderMan", "Matrix"]
-    },
-    {
-    nombre:"Lis",
-    edad : 35,
-    precio: 1500,
-    nombreSeries: ["Emily","TheOffice","JarriPoter"],
-    nombrePeliculas : ["Sascuach","SpiderMan", "Everlong"]
-    }
-]
-
-console.log("hola");
-console.log(usuarios.edad)
 
 //---------------- Repaso de info => .map
 
-let numeros = [1,2,3,4,5,6,7,8,9]
-let dobles = numeros.map(function(x){
-    return x*2;
-})
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let dobles = numeros.map(function (x) {
+	return x * 2;
+});
 
-console.log(numeros)
-console.log(dobles)
+console.log(numeros);
+console.log(dobles);
 
-let expoDobles = dobles.map((a,b)=>b**a)
-let expoDobles2 = dobles.map((a,b)=>a**b)
+let expoDobles = dobles.map((a, b) => b ** a);
+let expoDobles2 = dobles.map((a, b) => a ** b);
 
-console.log(expoDobles)
+console.log(expoDobles);
 
-console.log(expoDobles2)
+console.log(expoDobles2);
