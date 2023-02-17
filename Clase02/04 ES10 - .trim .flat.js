283da70242abe7acfcb26e54db4b0a327b5e8657
Diego Hernandez <dihf.47@gmail.com>
2023-02-16 22:18:18 -0300

    cambios

--- Clase02/04 ES10 - .trim .flat.js
+++ Clase02/04 ES10 - .trim .flat.js
@@ -1,12 +1,12 @@
-// --- .trim()
+// --- .trim() -  recorta los espacio inicial y final en un String
 
-const saludo = "     hola    todo el mundo"
+const saludo = "     hola todo el mundo         "
 
 console.log(saludo)
 console.log(saludo.trim())
 
 
-//--- .flat()
+//--- .flat() - desarma arrays dentros de un array
 
 const array =[1,2,3,4,[[5,6,7,8],[9,1,2,[3,4,5,6,7]]]]
 
