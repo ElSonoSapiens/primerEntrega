da02b175057ea571dd88be7c24f109ac9e29a4eb
ElSonoSapiens <dihf.47@gmail.com>
2023-03-15 14:35:18 -0300

    primer_entrega

--- src/CartManager.js
+++ src/CartManager.js
@@ -1,10 +1,47 @@
 import fs, { existsSync } from "fs";
 
-export default class ProductManager {
+export default class CartManager {
 	constructor(path) {
 		this.path = path;
 	}
 
+	async getCarts(){
+		if (fs.existsSync(this.path)) {
+			const cartsFile = await fs.promises.readFile(this.path,"utf-8")
+		return JSON.parse(cartsFile)
+	}else{
+		return[]
+	}
+}
+
+	async getCart(id){
+	const cartsFile = await this.getCarts()
+const cart = cartFile.find((cart)=> cart.id ===id)
+if(cart){
+	return cart
+}else{
+	return "Cart not found"
+}
+	async createCart(){
+		const cartsFile = await thjis.getCarts()
+		const newCart = {
+			id.this.#idGenerator(cartsFile)
+			products:[]
+		}
+	}
+	
+	#idGenerator = (carts) => {
+		let id = carts.length === 0 ? 1 : carts[carts.length - 1].id + 1;
+		return id;
+	};
+
+
+}
+
+
+
+
+	/*
 	async getProducts() {
 		if (fs.existsSync(this.path)) {
 			const products = await fs.promises.readFile(this.path, "utf-8");
@@ -92,3 +129,5 @@ export default class ProductManager {
 		return id;
 	};
 }
+
+*/
---
+++ src/routes/carts.router.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import CartManager from "../CartManager.js";
+import __dirname from "../utils.js"
+
+const carts = [];
+const router = Router();
+
+const CartManager = new ManagerCart (__dirname+"/carts.json")
+
+// crear carrito
+router.post("/", (req, res) => {
+	const newCart = await CartManager.createCart()
+  res.json = ({cart:newCart})
+});
+
+// buscar carrito
+router.get("/:cid", async (req, res) => {
+  const {cid} = req.params
+const cart = await CartManager.getCart(idCart)});
+res.json({cart})
+
+// agregar un producto al array del carrito
+router.post("/:pid", (req, res) => {
+	const obj = req.body;
+	carts.push(obj);
+	res.json({ message: "Cart added" });
+});
+
+//router.put()
+
+//router.delete()
+
+export default router;
